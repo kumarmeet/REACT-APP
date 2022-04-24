0182b9eb433311a9e0b299eaedb9cc62191c608d
kumarmeet <kumarmeet51@yahoo.com>
2022-04-24 14:03:22 +0530

    used two way binding with form input. reset the value of input using useState() 1 index variable and set property into value with useState() 0 index varibale it will first get the value and reset the value whenever form is submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,40 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+
+import moment from "moment";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("")
+  const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value.toString())
-  }
+    setEnteredTitle(event.target.value.toString());
+  };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(+event.target.value)
-  }
+    setEnteredAmount(event.target.value.toString());
+  };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value.toString())
-  }
+    setEnteredDate(event.target.value.toString());
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: moment(enteredDate).format("DD MMMM YYYY"),
+    };
+
+    //two way binding first set the value and reset the value in the state variables
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler}/>
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-04-23" max="2025-04-23" onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2022-04-23"
+            max="2025-04-23"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+          />
         </div>
       </div>
 
