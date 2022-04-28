bfaa221aae7297b00e0684efc323ba3866d03553
kumarmeet <kumarmeet51@yahoo.com>
2022-04-28 23:55:00 +0530

    added components, ui components and fundamental features like useState, resetting value, props and so on

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import AddUser from "./components/Users/AddUser";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    if (
+      userName.trim().length === 0 ||
+      userAge.trim().length === 0 ||
+      +userAge < 1
+    ) {
+      return;
+    }
+
+    console.log(userName, userAge);
+    setUserName("");
+    setUserAge("");
+  };
+
+  const userNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const userAgeHandler = (event) => {
+    setUserAge(event.target.value);
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input
+          onChange={userNameHandler}
+          value={userName}
+          id="username"
+          type="text"
+        />
+
+        <label htmlFor="age">Age (in years)</label>
+        <input
+          onChange={userAgeHandler}
+          value={userAge}
+          id="age"
+          type="number"
+        />
+
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
